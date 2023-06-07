f27b8cbceaf34d5f4de3b388348743811a8ea08b
TheophileDesmedt <theophiledesmedt@gmail.com>
2023-06-07 22:42:49 +0200

    correction text previous button in header component

--- src/app/components/page-header/page-header.component.ts
+++ src/app/components/page-header/page-header.component.ts
@@ -13,7 +13,7 @@ import { IonicModule } from '@ionic/angular';
 export class PageHeaderComponent  implements OnInit {
 
   @Input() pageTitle: string = 'Title of the current page';
-  @Input() previousPage: string = '';
+  @Input() previousButtonText: string = '';
 
   constructor() { }
 
