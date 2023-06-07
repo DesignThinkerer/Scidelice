c07003a2bfe030300c27beb5fe7ea0cb1c61fa42
TheophileDesmedt <theophiledesmedt@gmail.com>
2023-06-07 22:26:44 +0200

    login page

--- src/app/pages/login/login.page.spec.ts
+++ src/app/pages/login/login.page.spec.ts
@@ -1,19 +1,43 @@
-//TODO: solve async issue
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
 
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
-// import { LoginPage } from './login.page';
+import { LoginPage } from './login.page';
 
-// describe('LoginPage', () => {
-//   let component: LoginPage;
-//   let fixture: ComponentFixture<LoginPage>;
+describe('LoginPage', () => {
+  let component: LoginPage;
+  let fixture: ComponentFixture<LoginPage>;
+  let router: Router;
 
-//   beforeEach(async(() => {
-//     fixture = TestBed.createComponent(LoginPage);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   }));
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [IonicModule.forRoot(), LoginPage],
+    }).compileComponents();
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+    fixture = TestBed.createComponent(LoginPage);
+    router = TestBed.inject(Router);
+    component = fixture.componentInstance;
+  });
+
+  it('should go to home page on login', (): void => {
+
+    // expect(true).toBeFalsy(); TDD: always write a test that fails first to make sure the test is working
+
+    spyOn(router, 'navigate');
+
+    component.ngOnInit();
+    component.login();
+
+    expect(router.navigate).toHaveBeenCalledWith(['home']);
+
+  });
+  it('should go to home page on register', (): void => {
+    spyOn(router, 'navigate');
+
+    component.ngOnInit();
+    component.register();
+
+    expect(router.navigate).toHaveBeenCalledWith(['home']);
+
+  });
+});
