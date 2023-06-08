8460ea5bf92ee37ba19ecd0f44932584bef7d5b8
TheophileDesmedt <theophiledesmedt@gmail.com>
2023-06-08 17:38:40 +0200

    updated buttons for import / export

--- src/app/pages/recipes/new.recipe/new.recipe.page.ts
+++ src/app/pages/recipes/new.recipe/new.recipe.page.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule, NgIf } from '@angular/common';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { Router } from '@angular/router';
 
@@ -15,24 +20,34 @@ import { NewRecipeForm } from './new.recipe.form';
   templateUrl: './new.recipe.page.html',
   styleUrls: ['./new.recipe.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule, PageHeaderComponent, ReactiveFormsModule, NgIf]
+  imports: [
+    IonicModule,
+    CommonModule,
+    FormsModule,
+    PageHeaderComponent,
+    ReactiveFormsModule,
+    NgIf,
+  ],
 })
 export class NewRecipePage {
-
   form!: FormGroup;
 
-  constructor(private router: Router, private formBuilder: FormBuilder) { 
+  constructor(private router: Router, private formBuilder: FormBuilder) {
     this.form = new NewRecipeForm(this.formBuilder).createForm();
   }
 
   createRecipe() {
-    console.log("createRecipe");
+    console.log('createRecipe');
     this.router.navigate(['/recipes']);
   }
 
-  isSupported= false;
-  scan(){
-    console.log("scan begin");
-}
+  isSupported = false;
+  importRecipe() {
+    console.log('import recipe file');
+  }
 
+  scan() {
+    console.log('scan begin');
+    this.importRecipe();
+  }
 }
