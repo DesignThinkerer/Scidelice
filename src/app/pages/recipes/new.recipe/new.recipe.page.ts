6fdf76e8bc7759f02be39d1cda2db1fcf1e18d06
TheophileDesmedt <theophiledesmedt@gmail.com>
2023-06-08 12:44:21 +0200

    add basic form logic

--- src/app/pages/recipes/new.recipe/new.recipe.page.ts
+++ src/app/pages/recipes/new.recipe/new.recipe.page.ts
@@ -1,21 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { Router } from '@angular/router';
+
+//standalone components
 import { PageHeaderComponent } from 'src/app/components/page.header/page.header.component';
+
+//tools
+import { NewRecipeForm } from './new.recipe.form';
 
 @Component({
   selector: 'app-new-recipe',
   templateUrl: './new.recipe.page.html',
   styleUrls: ['./new.recipe.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule, PageHeaderComponent]
+  imports: [IonicModule, CommonModule, FormsModule, PageHeaderComponent, ReactiveFormsModule]
 })
 export class NewRecipePage implements OnInit {
 
-  constructor() { }
+  form!: FormGroup;
 
-  ngOnInit() {
+  constructor(private router: Router, private formBuilder: FormBuilder) { 
+    this.form = new NewRecipeForm(this.formBuilder).createForm();
+  }
+
+  ngOnInit() {}
+
+  createRecipe() {
+    console.log("createRecipe");
+    this.router.navigate(['/recipes']);
   }
 
 }
--- src/app/pages/recipes/recipes.page.spec.ts
+++ src/app/pages/recipes/recipes.page.spec.ts
@@ -1,3 +1,4 @@
+//TODO: see https://youtu.be/3gaVbroD-l8?t=634 for an example
 // import { ComponentFixture, TestBed } from '@angular/core/testing';
 // import { RecipesPage } from './recipes.page';
 
